ea9d00e0b5dcfe53c45f49b0a46396640636b505
Kosuke Tomita <s163404@gmail.com>
2021-09-12 19:53:37 +0900

    角丸のビューを追加（コードは未変更）

---
+++ Todo/RoundedCorners.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedCorners.swift
+//  Todo
+//
+//  Created by 冨田康介 on 2021/09/12.
+//
+
+import SwiftUI
+
+struct RoundedCorners: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoundedCorners_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundedCorners()
+    }
+}
