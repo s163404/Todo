db63a71e4fb3e31fa51485fb91842cdf1b31abee
Kosuke Tomita <s163404@gmail.com>
2021-09-22 00:12:34 +0900

    Gradient構造体を使って色のグラデーションをつける

--- Todo/CategoryView.swift
+++ Todo/CategoryView.swift
@@ -18,7 +18,12 @@ struct CategoryView: View {
     }
     
     var body: some View {
-        VStack(alignment: .leading) {
+        
+        let gradient = Gradient(colors: [category.color(), category.color().opacity(0.8)])
+        let linearGradationColor = LinearGradient(gradient: gradient, startPoint: .top, endPoint: .bottom)
+        
+        // memo: 定数があるため、returnを明示的につける必要がある
+        return VStack(alignment: .leading) {
             Image(systemName: category.image())
                 .font(.largeTitle)
                 // 指定したcontentをシート表示する
@@ -44,7 +49,7 @@ struct CategoryView: View {
         .padding()
         .frame(maxWidth: .infinity, minHeight:  150)
         .foregroundColor(.white)
-        .background(category.color())
+        .background(linearGradationColor)
         .cornerRadius(20)
         .onTapGesture {
             self.showList = true
